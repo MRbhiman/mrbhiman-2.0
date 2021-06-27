e85ce9ba03f2073e847d9b45aaecb4861fffdf7a
『ϻᴿ』☯Sʜᴀᴅօw <86451921+MRbhiman@users.noreply.github.com>
2021-06-27 20:25:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ def help(updater,context):
 
 def add_group(update: Update, context: CallbackContext):
     for member in update.message.new_chat_members:
-        update.message.reply_text(f'Hello {member.full_name} , Welcome to ln support Thank you for Joining  ')
+        update.message.reply_text(f'Hello {member.full_name} , ഞങ്ങളുടെ ഗ്രൂപ്പിൽ ചേരുന്നതിന് നന്ദി ❤️..  ')
 
 add_group_handle = MessageHandler(Filters.status_update.new_chat_members, add_group)
 updater.dispatcher.add_handler(add_group_handle)
